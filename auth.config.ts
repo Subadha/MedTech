367cb6c8769e68ff4bc928ed998b88665270f22a
vignesh <vigneshten5@gmail.com>
2024-07-25 20:08:30 +0530

    6

--- auth.config.ts
+++ auth.config.ts
@@ -1,40 +1,46 @@
-import type { NextAuthConfig } from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import { LoginSchema } from "./schema"
+import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "./schema";
 import { getUserByEmail } from "./data/user";
 import bcrypt from "bcryptjs";
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export default {
   providers: [
     Google({
-      clientId:process.env.GOOGLE_CLIENT_ID,
-      clientSecret:process.env.GOOGLE_CLIENT_SECRET
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     GitHub({
-      clientId:process.env.GITHUB_CLIENT_ID,
-      clientSecret:process.env.GITHUB_CLIENT_SECRET
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
     Credentials({
-        async authorize(credentials){
-            const validateFields = LoginSchema.safeParse(credentials);
-            if(validateFields.success){
-                const {email,password} = validateFields.data;
+      async authorize(credentials, request) {
+        if (!request) {
+          throw new Error("Request object not found.");
+        }
 
-                const user = await getUserByEmail(email);
-                if(!user || !user.password) return null;
+        const validateFields = LoginSchema.safeParse(credentials);
+        if (validateFields.success) {
+          const { email, password } = validateFields.data;
 
-                const passwordMatch = await bcrypt.compare(
-                    password,
-                    user.password
-                );
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
 
-                if(passwordMatch) return user;
+          const passwordMatch = await bcrypt.compare(
+            password,
+            user.password
+          );
 
-                return null;
-            }
-        } 
-    })
+          if (passwordMatch) return user;
+
+          return null;
+        }
+
+        return null;
+      },
+    }),
   ],
-} satisfies NextAuthConfig
+} as NextAuthConfig;
