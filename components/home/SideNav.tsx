8e2e1fcfdec1100f64a4e2e6047733149496cc67
meadarsh <itsadarsh33@gmail.com>
2024-08-10 18:13:26 +0530

    Conselt page

--- components/home/SideNav.tsx
+++ components/home/SideNav.tsx
@@ -1,84 +1,103 @@
 "use client";
-import { auth, signOut } from "@/auth";
 import React from "react";
-import { Menu, X } from "lucide-react";
+import { icons, LogOut } from "lucide-react";
 import Image from "next/image";
 import logo from "@/app/images/logo.png";
 import Link from "next/link";
 import { SignOut } from "@/actions/signout";
-import { SearchBar } from "../dashboard/search-bar";
-import Header from "../dashboard/header";
+import { usePathname } from "next/navigation";
 
-export const menuItems = [
+interface MenuItem {
+  name: string;
+  icon: keyof typeof icons; 
+  href: string;
+}
+
+export const menuItems: MenuItem[] = [
   {
     name: "Dashboard",
+    icon: "LayoutDashboard",
     href: "/dashboard",
   },
   {
     name: "Appointment",
+    icon: "UserRoundCheck",
     href: "/dashboard/appointment",
   },
   {
+    name: "Consult",
+    icon: "BriefcaseMedical",
+    href: "/dashboard/consult",
+  },
+  {
     name: "Calendar",
+    icon: "Calendar",
     href: "/dashboard/calender",
   },
   {
     name: "Book",
+    icon: "UserRoundCheck",
     href: "/dashboard/book",
   },
   {
     name: "Profile",
+    icon: "CircleUserRound",
     href: "/dashboard/profile",
   },
   {
     name: "Help",
+    icon: "MessageCircleQuestion",
     href: "/dashboard/help",
   },
 ];
 
-// Define the props type
 type SideNavProps = {
   userName: string;
   role: string;
 };
 
-export default function SideNav({ userName, role}: SideNavProps) {
+export default function SideNav({ userName, role }: SideNavProps) {
+  const location = usePathname();
   const handleLogout = async (e: React.FormEvent) => {
     e.preventDefault();
     await SignOut(); // Sign out the user
   };
 
   return (
-      <aside
-        className={` lg:block hidden h-full w-64`}
-      >
-        <div className="flex items-center justify-between p-4 border-b border-gray-700">
-          <div className="flex items-center">
-            <Link href="/">
-              {" "}
-              <Image src={logo} alt="logo" width={100} height={40} />
-            </Link>
-          </div>
+    <aside className={`lg:block hidden h-full w-64`}>
+      <div className="flex items-center justify-between p-4">
+        <div className="flex items-center">
+          <Link href="/">
+            <Image src={logo} alt="logo" width={70} height={70} />
+          </Link>
         </div>
-        <nav className="p-4 space-y-2">
-          {menuItems.map((item) => (
+      </div>
+      <nav className="p-4 space-y-2">
+        {menuItems.map((item) => {
+          const Icon = icons[item.icon];
+          return (
             <a
               key={item.name}
               href={item.href}
-              className="block py-2 px-3 rounded hover:bg-purple-700"
+              className={`flex gap-2 py-2 px-3 rounded ${item.href === location ? "bg-primary text-white" : ""}`}
             >
+              <span>
+                <Icon className="w-5" />
+              </span>
               {item.name}
             </a>
-          ))}
-          <form onSubmit={handleLogout} className="mt-4">
-            <button
-              type="submit"
-              className="w-full py-2 px-3 text-left rounded hover:bg-purple-700"
-            >
-              Sign Out
-            </button>
-          </form>
-        </nav>
-      </aside>
+          );
+        })}
+        <form onSubmit={handleLogout} className="mt-4">
+          <button
+            type="submit"
+            className="w-full flex gap-2 py-2 px-3 text-left rounded hover:bg-primary hover:text-white"
+          >
+            <LogOut className="w-5" />
+            Sign Out
+          </button>
+        </form>
+      </nav>
+    </aside>
   );
 }
