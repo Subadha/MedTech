87fa82d33c2ec8afb344943d8e0dfbb0ac4e7937
meadarsh <itsadarsh33@gmail.com>
2024-08-16 19:17:56 +0530

    Doctor dashboard

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -3,23 +3,46 @@ import { MainCarausel } from "@/components/dashboard/carausel";
 import NearbyDoctors from "@/components/dashboard/nearby-doctors";
 import RecommendedDoctors from "@/components/dashboard/recommended-doctor";
 import UpcomingAppointments from "@/components/dashboard/upcoming-appointments";
+import CalenderAndAppointments from "@/components/doctor-dashboard/doc-calender";
+import { DocCarausel } from "@/components/doctor-dashboard/doc-Carausel";
+import PatientList from "@/components/doctor-dashboard/doc-patientList";
 
 const Settings = async () => {
-    const session = await auth();
-    const id = session?.user.id  
+  const session = await auth();
+  const id = session?.user.id;
+  console.log(session?.user.role);
+  const role = session?.user.role;
 
-    return (
-        <div className="p-3 grid gap-4 grid-cols-6">
-           <div className=" col-span-6 lg:col-span-4">
-           <MainCarausel/>
-           <NearbyDoctors/>
-           </div>
-            <UpcomingAppointments id={id}/>
-            <RecommendedDoctors/>
-        </div>
-    );
-}
+  return (
+    <div className="p-3 grid gap-4 grid-cols-6">
+      {role === "USER" ? <User id={id} /> : <Doctor id={id} />}
+    </div>
+  );
+};
 
 export default Settings;
 
+const User = ({ id }: any) => {
+  return (
+    <>
+      <div className=" col-span-6 lg:col-span-4">
+        <MainCarausel />
+        <NearbyDoctors />
+      </div>
+      <UpcomingAppointments id={id} />
+      <RecommendedDoctors />
+    </>
+  );
+};
 
+const Doctor = ({ id }: any) => {
+  return (
+    <>
+      <div className=" col-span-6 lg:col-span-4">
+        <DocCarausel />
+        <PatientList />
+      </div>
+      <CalenderAndAppointments />
+    </>
+  );
+};
