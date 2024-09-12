f98c7f872d6be9ac8e56441d9fadd2d6c328c45d
meadarsh <itsadarsh33@gmail.com>
2024-09-13 02:14:09 +0530

    Landingpage update

--- components/home/Team.tsx
+++ components/home/Team.tsx
@@ -2,8 +2,17 @@
 import { useState, useEffect } from "react";
 import { FaLinkedin, FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import Image from "next/image";
-import Link from 'next/link';
-import { motion } from "framer-motion";
+import Link from "next/link";
+import * as React from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import emblaCarouselAutoplay from "embla-carousel-autoplay";
 
 // Import images
 import img1 from "@/app/images/program-1.jpg";
@@ -13,128 +22,108 @@ import img4 from "@/app/images/program-4.jpeg";
 import img5 from "@/app/images/program-5.jpg";
 
 export default function Team() {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [visibleItems, setVisibleItems] = useState(3);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [visibleItems, setVisibleItems] = useState(3);
 
-    const teamMembers = [
-        {
-            name: "Gurushankar",
-            title: "Founder",
-            image: img1,
-            linkedinUrl: "https://www.linkedin.com/in/gurushankar-ajikumar-8073761a5/"
-        },
-        {
-            name: "Veena C",
-            title: "Consultant and Cofounder",
-            image: img2,
-            linkedinUrl: "https://www.linkedin.com/in/veena-c-1986aa22b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
-        },
-        {
-            name: "Renu John",
-            title: "Mentor: Professor & Head CfHE, IITH",
-            image: img3,
-            linkedinUrl: "https://www.linkedin.com/in/renu-john-ab20a29/"
-        },
-        {
-            name: "Sushmee Badhulika",
-            title: "Scientific advisor: Professor IITH",
-            image: img4,
-            linkedinUrl: "https://www.linkedin.com/in/sushmee-badhulika-48422433/"
-        },
-        {
-            name: "Neeko Inees Chiriyankandath",
-            title: "Scientific Advisor: Consultant Gynaecologist",
-            image: img5,
-            linkedinUrl: "https://www.linkedin.com/in/dr-neeko-inees-chiriyankdath-2b79a0196/"
-        },
-    ];
+  const teamMembers = [
+    {
+      name: "Gurushankar",
+      title: "Founder",
+      image: img1,
+      linkedinUrl:
+        "https://www.linkedin.com/in/gurushankar-ajikumar-8073761a5/",
+    },
+    {
+      name: "Veena C",
+      title: "Consultant and Cofounder",
+      image: img2,
+      linkedinUrl:
+        "https://www.linkedin.com/in/veena-c-1986aa22b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      name: "Renu John",
+      title: "Mentor: Professor & Head CfHE, IITH",
+      image: img3,
+      linkedinUrl: "https://www.linkedin.com/in/renu-john-ab20a29/",
+    },
+    {
+      name: "Sushmee Badhulika",
+      title: "Scientific advisor: Professor IITH",
+      image: img4,
+      linkedinUrl: "https://www.linkedin.com/in/sushmee-badhulika-48422433/",
+    },
+    {
+      name: "Neeko Inees Chiriyankandath",
+      title: "Scientific Advisor: Consultant Gynaecologist",
+      image: img5,
+      linkedinUrl:
+        "https://www.linkedin.com/in/dr-neeko-inees-chiriyankdath-2b79a0196/",
+    },
+  ];
 
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth < 640) {
-                setVisibleItems(1);
-            } else if (window.innerWidth < 1024) {
-                setVisibleItems(2);
-            } else {
-                setVisibleItems(3);
-            }
-        };
-
-        window.addEventListener("resize", handleResize);
-
-        // Initial check
-        handleResize();
-
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
-    const nextSlide = () => {
-        setCurrentIndex((prevIndex) =>
-            prevIndex < teamMembers.length - visibleItems ? prevIndex + 1 : prevIndex
-        );
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 640) {
+        setVisibleItems(1);
+      } else if (window.innerWidth < 1024) {
+        setVisibleItems(2);
+      } else {
+        setVisibleItems(3);
+      }
     };
 
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : prevIndex));
-    };
+    window.addEventListener("resize", handleResize);
 
-    return (
-        <motion.div
-            initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0, transition: { duration: 1 } }}
-            viewport={{ once: true }}
-            className="w-full overflow-hidden"
-        >
-            <div id="team" className="pb-[100px] sm:pb-10 flex flex-col justify-center items-center text-center w-full overflow-hidden">
-                <h1 className="sm:text-5xl text-4xl font-bold pb-[100px]">
-                    Innovation at the heart of health: Meet our Visionary Team
-                </h1>
-                <div className="relative flex flex-col items-center w-full overflow-hidden">
-                    <div className="relative flex items-center w-full px-4 sm:px-12 mx-4 sm:mx-10">
-                        <div className="flex overflow-hidden w-full">
-                            <div
-                                className="flex transition-transform duration-500 ease-in-out"
-                                style={{ transform: `translateX(-${currentIndex * (100 / visibleItems)}%)` }}
-                            >
-                                {teamMembers.map((member, index) => (
-                                    <div key={index} className="flex-none w-[calc(100%/1.5)]  sm:w-[calc(100%/3)] px-2 sm:px-5 box-border">
-                                        <div className="relative w-full h-[300px] sm:h-[400px] rounded-2xl overflow-hidden">
-                                            <Image
-                                                src={member.image}
-                                                alt={member.name}
-                                                layout="fill"
-                                                objectFit="cover"
-                                                className="rounded-2xl"
-                                            />
-                                            <Link href={member.linkedinUrl} passHref legacyBehavior>
-                                                <a rel="noopener noreferrer" className="absolute top-0 right-0 p-3">
-                                                    <FaLinkedin size={30} className="text-blue-600" />
-                                                </a>
-                                            </Link>
-                                        </div>
-                                        <div className="w-full bg-white text-black justify-center text-center items-center h-[70px] mt-2 rounded-md flex flex-col">
-                                            <span className="font-bold text-xl">{member.name}</span>
-                                            <span>{member.title}</span>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
+    // Initial check
+    handleResize();
 
-                    <div className="absolute flex justify-between items-center w-full top-1/2 transform -translate-y-1/2 px-4"
-                        style={{ padding: '0 16px' }}>
-                        <FaArrowLeft
-                            className="cursor-pointer text-3xl z-10"
-                            onClick={prevSlide}
-                        />
-                        <FaArrowRight
-                            className="cursor-pointer text-3xl z-10"
-                            onClick={nextSlide}
-                        />
-                    </div>
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+        loop: true,
+      }}
+      plugins={[
+        emblaCarouselAutoplay({
+          delay: 6000,
+        }),
+      ]}
+      className="lg:w-[80%] my-10 mx-auto w-[85%]"
+    >
+      <CarouselContent>
+        {teamMembers.map((_, index) => (
+          <CarouselItem
+            key={index}
+            className="md:basis-1/3 sm:basis-1/2 xl:basis-1/4 w-full"
+          >
+            <Card className="rounded-lg h-full border backdrop-blur-sm bg-[rgba(255,255,255,.1)]">
+              <CardContent className="flex aspect-square flex-col lg:p-3 p-2">
+                <Image
+                  className="h-full w-full rounded-lg object-cover"
+                  width={333}
+                  height={218}
+                  src={_.image}
+                  alt="Mentor Image"
+                />
+                <div className=" w-full relative py-2 px-1">
+                  <Link  href={_.linkedinUrl} passHref legacyBehavior>
+                    <a className="absolute -top-4 right-2" target="_blank" rel="noopener noreferrer">
+                      <FaLinkedin size={30} className="text-blue-600 bg-white rounded" />
+                    </a>
+                  </Link>
+                  <h2 className="font-semibold">{_.name}</h2>
+                  <p className=" text-sm">{_.title}</p>
                 </div>
-            </div>
-        </motion.div>
-    );
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="md:block hidden" />
+      <CarouselNext className="md:block hidden" />
+    </Carousel>
+  );
 }
