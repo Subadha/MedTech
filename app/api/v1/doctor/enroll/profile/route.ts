eb7ee410e8c9c046498b57a9974c585c99830d14
meadarsh <itsadarsh33@gmail.com>
2024-09-23 14:42:19 +0530

    fixed

--- app/api/v1/doctor/enroll/profile/route.ts
+++ app/api/v1/doctor/enroll/profile/route.ts
@@ -1,31 +1,33 @@
 import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-export const POST =async(req:any)=>{
+export const POST = async (req: any) => {
   try {
-    const body =await req.json()
-    const {values,id}=body;    
+    const body = await req.json();
+    const { values, id } = body;
     const user = await getUserById(id);
     if (!user) {
-      return { error: "User not found" };
+      return NextResponse.json({ error: "User not found" });
     }
 
     const existingProfile = await db.doctorProfile.findFirst({
       where: { userId: id },
     });
     if (existingProfile) {
-      return { error: "Doctor profile already exists for this user" };
+      return NextResponse.json({
+        error: "Doctor profile already exists for this user",
+      });
     }
 
-   const result= await db.doctorProfile.create({
+    const result = await db.doctorProfile.create({
       data: {
         ...values,
         userId: id,
       },
     });
-    return NextResponse.json(result)
+    return NextResponse.json(result);
   } catch (error) {
-    return NextResponse.json(error)
+    return NextResponse.json(error);
   }
-}
+};
