26861c0801096e449208253ee885edf2a620cba2
meadarsh <itsadarsh33@gmail.com>
2024-09-30 15:36:28 +0530

    Fix

--- components/appointment/RescheduleSheet.tsx
+++ components/appointment/RescheduleSheet.tsx
@@ -37,12 +37,12 @@ export function RescheduleSheet({ open, close }: any) {
             <Input id="name" value="Pedro Duarte" className="col-span-3" />
             <div className="flex flex-col gap-2">
               <h3 className="text-xl font-bold">Select date</h3>
-              <DatePickerDemo
+              {/* <DatePickerDemo
                 setDate={(e: any) => form.setValue("date", e)} // Set date on selection
                 availableDays={
                   details?.doctorAvailabilityDetails?.availableDays
                 }
-              />
+              /> */}
             </div>
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
