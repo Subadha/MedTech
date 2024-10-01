cb14fc89ba967462fa8a0fd438337aee02547285
meadarsh <itsadarsh33@gmail.com>
2024-10-02 02:29:36 +0530

    Added rating avg and total

--- actions/consult/GetDoctorById.ts
+++ actions/consult/GetDoctorById.ts
@@ -7,23 +7,38 @@ export const GetDoctorById = async (id: string) => {
     const doctor = await db.user.findFirst({
       where: {
         id: id,
-        role: 'DOCTOR',
+        role: "DOCTOR",
       },
       include: {
         doctorProfile: true,
         doctorAvailabilityDetails: true,
       },
-      
     });
+    const reviews = await db.reviews.findMany({
+      where: {
+        userId: id,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    // Calculate total reviews and average rating
+    const totalReviews = reviews.length;
+    const avgRating =
+      totalReviews > 0
+        ? reviews.reduce((sum, review) => sum + parseFloat(review.rating), 0) /
+          totalReviews
+        : 0;
 
     if (!doctor) {
       console.error(`Doctor with ID ${id} not found.`);
       return null;
     }
 
-    return doctor;
+    return {doctor,totalReviews, avgRating};
   } catch (error) {
-    console.error('Error fetching doctor with details:', error);
+    console.error("Error fetching doctor with details:", error);
     return null;
   }
 };
