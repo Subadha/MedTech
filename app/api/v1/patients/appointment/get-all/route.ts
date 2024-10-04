04904e3ff1ca09b4335946784f1a7eea99f05bfb
meadarsh <itsadarsh33@gmail.com>
2024-10-04 18:55:40 +0530

    Update

--- app/api/v1/patients/appointment/get-all/route.ts
+++ app/api/v1/patients/appointment/get-all/route.ts
@@ -1,26 +1,28 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export const POST=async (req:any)=>{
-    try{
-        const request =await req.json()
-        const {userId,status}=request
-        const query = {
-          where: {
-            userId: userId,
-            ...(status && { status }),  
-          },
-        };
-        const appointments = await db.bookedAppointment.findMany(query);
-          if (!appointments || appointments?.length === 0) {
-            return NextResponse.json({error: "No appointments found for this user." });
-          }
-          
-          return NextResponse.json({ success:"Successfully fetched", data: appointments });
+export const POST = async (req: any) => {
+  try {
+    const request = await req.json();
+    const { userId, status } = request;
+
+    // Build query object conditionally, handle multiple statuses if passed as an array
+    const query = {
+      where: {
+        userId: userId,
+        ...(status && { status: Array.isArray(status) ? { in: status } : status }),  // Use 'in' for multiple statuses
+      },
+    };
+
+    const appointments = await db.bookedAppointment.findMany(query);
+
+    if (!appointments || appointments.length === 0) {
+      return NextResponse.json({ error: "No appointments found for this user." });
     }
-    catch (err) {
-        console.log(err);
-        
-        return NextResponse.json({error:"Internal server error"})
-    }
-}
+
+    return NextResponse.json({ success: "Successfully fetched", data: appointments });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ error: "Internal server error" });
+  }
+};
