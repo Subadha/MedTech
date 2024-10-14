ba2c5aa70bde3d5a6a50b6433eaa63bd785cc96a
meadarsh <itsadarsh33@gmail.com>
2024-10-14 16:45:23 +0530

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <link rel="preload" href="/_next/static/media/a34f9d1faa5f3315-s.p.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </head>
       <body className={inter.className}>
