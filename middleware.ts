b9958f513b89188e36f90b6aa85bbdd4b7f56204
meadarsh <itsadarsh33@gmail.com>
2024-12-30 15:55:24 +0530

    update

--- middleware.ts
+++ middleware.ts
@@ -28,7 +28,7 @@ export default auth((req)=>{
         return ;
     }
     if(!isLoggedIn && !isPublicRoute()){        
-        return Response.redirect(new URL("/auth/login",nextUrl));
+        return Response.redirect(process.env.NEXT_PUBLIC_URL+"/auth/login");
     }
     
     if(isApiAuthRoute){
